76fd1e2d8ddcedd052617335b84a6c4b8926a889
mgolw001 <mgolw001@ucr.edu>
2021-10-15 02:35:11 -0700

    Commit test.cpp

--- test.cpp
+++ test.cpp
@@ -27,16 +27,11 @@ TEST(EchoTest, Number) {
 TEST(EchoTest, SpecialChar) {
     char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "$";
     EXPECT_EQ("$", echo(2,test_val));
->>>>>>> master
 }
 
 
 int main(int argc, char **argv) {
-<<<<<<< HEAD
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-=======
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();}
 
